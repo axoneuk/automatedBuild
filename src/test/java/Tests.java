fcf3dfc160a4afa7946f3dc7327c102d84d8d9bb
axoneuk <info@axone-tech.uk>
2020-04-05 08:24:10 +0100

    jira 65263 - Setup 4 tests to fail..

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class Tests {
@@ -5,21 +6,25 @@ public class Tests {
     @Test
     public void test1(){
         System.out.println("running test 1");
+        Assert.fail();
     }
 
     @Test
     public void test2(){
         System.out.println("running test 2   ");
+        Assert.fail();
     }
 
     @Test
     public void test3(){
         System.out.println("running test 3");
+        Assert.fail();
     }
 
     @Test
     public void test4(){
         System.out.println("running test 4");
+        Assert.fail();
     }
 
 }
